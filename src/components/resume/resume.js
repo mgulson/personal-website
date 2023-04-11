e29e67e607b1b02f5eda703a05fb608d9006e91f
Michael Gulson <mgulson@optoro.com>
2023-04-10 22:23:44 -0400

    Use Material UI and grids

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import Headshot from '../../img/michael-gulson-headshot.JPG'
-
+import Grid from '@mui/material/Grid'
 class Resume extends React.Component  {
   render() {
     return(
       <>
-      <img className="headshot" src={Headshot} alt="Michael Gulson headshot" width="300" height="400"></img>
-      <div>
-        <span>Michael Gulson</span>
-        <br></br>
-        <span>Software Engineer</span>
-      </div>
-      </>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <img className="headshot" src={Headshot} alt="Michael Gulson headshot" width="300" height="400"></img>
+        </Grid>
+        <Grid item xs={8}>
+          <h1> Michael Gulson's Website</h1>
+          <h2> Software Engineer</h2>
+        </Grid>
+      </Grid>
+    </>
     )
   }
 }
