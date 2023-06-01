b9b4f20435b3f98def1bd312fd64347a30fbac9c
Michael Gulson <msgulson@gmail.com>
2023-06-01 18:58:15 -0500

    start work centering mobile

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
 import Headshot from '../../img/michael-gulson-headshot.JPG'
 import Grid from '@mui/material/Grid'
 import './Resume.css'
@@ -9,24 +8,28 @@ class Resume extends React.Component  {
       <>
       <Grid container spacing={2}>
         <Grid item xs={12} md={4}>
-          <img className="headshot" src={Headshot} alt="Michael Gulson headshot" width="300" height="400"></img>
+          <img className="headshot mobile-image" src={Headshot} alt="Michael Gulson headshot" width="300" height="400"></img>
         </Grid>
         <Grid item xs={12} md={8}>
-          <h1> Michael Gulson</h1>
-          <h2> Software Engineer</h2>
-          <span>msgulson@gmail.com 8473238610</span>
-          <br></br>
-          <h2>Work Experience</h2>
-          <h3 className='company'>Optoro</h3> <span>Nov 2021 - Present</span>
-          <br></br> <br></br>
-          <h3 className='company'>Faraday Software</h3> <span>Sep 2020 - Nov 2021</span>
-          <br></br><br></br>
-          <h3 className='company'>Special Fitness</h3> <span>May 2020 - Sep 2020</span>
-          <br></br>
-          <br></br>
-          <a href="https://www.linkedin.com/in/michael-gulson-578270111" target="_blank">LinkedIn</a>
-          <span>                                                          </span>
-          <a href="https://www.github.com/michaelgulson" target="_blank">Github</a>
+          <div className='center'>
+            <div className= 'center-text'>
+              <h1> Michael Gulson</h1>
+              <h2> Software Engineer</h2>
+              <span>msgulson@gmail.com 8473238610</span>
+              <br></br>
+              <h2>Work Experience</h2>
+              <h3 className='company'>Optoro</h3> <span>Nov 2021 - Present</span>
+              <br></br> <br></br>
+              <h3 className='company'>Faraday Software</h3> <span>Sep 2020 - Nov 2021</span>
+              <br></br><br></br>
+              <h3 className='company'>Special Fitness</h3> <span>May 2020 - Sep 2020</span>
+              <br></br>
+              <br></br>
+              <a href="https://www.linkedin.com/in/michael-gulson-578270111" target="_blank">LinkedIn</a>
+              <span>                                                          </span>
+              <a href="https://www.github.com/michaelgulson" target="_blank">Github</a>
+            </div>
+          </div>
         </Grid>
       </Grid>
     </>
