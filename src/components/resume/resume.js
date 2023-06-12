8fa2ef5ef7396c36902bfcc90b99d9350d171cc1
Michael Gulson <msgulson@gmail.com>
2023-06-11 22:38:19 -0500

    fix resume mobile breakpoints

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -16,15 +16,11 @@ class Resume extends React.Component  {
               <h1> Michael Gulson</h1>
               <h2> Software Engineer</h2>
               <span>msgulson@gmail.com 8473238610</span>
-              <br></br>
               <h2>Work Experience</h2>
-              <h3 className='company'>Optoro</h3> <span>Nov 2021 - Present</span>
-              <br></br> <br></br>
-              <h3 className='company'>Faraday Software</h3> <span>Sep 2020 - Nov 2021</span>
+              <h3 className='company'>Optoro</h3> <span className='dates'>Nov 2021 - Present</span>
+              <h3 className='company'>Faraday Software</h3> <span className='dates'>Sep 2020 - Nov 2021</span>
+              <h3 className='company'>Special Fitness</h3> <span className='dates'>May 2020 - Sep 2020</span>
               <br></br><br></br>
-              <h3 className='company'>Special Fitness</h3> <span>May 2020 - Sep 2020</span>
-              <br></br>
-              <br></br>
               <a href="https://www.linkedin.com/in/michael-gulson-578270111" target="_blank">LinkedIn</a>
               <span>                                                          </span>
               <a href="https://www.github.com/michaelgulson" target="_blank">Github</a>
