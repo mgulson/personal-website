dfda2ccb41fddaed005e63225035281616b9bea8
Michael Gulson <msgulson@gmail.com>
2023-07-31 21:12:29 -0500

    made some updates need cors

--- src/components/CodingLanguages/CodingLanguages.js
+++ src/components/CodingLanguages/CodingLanguages.js
@@ -1,24 +1,42 @@
 import React from 'react';
 import { Formik, Field, Form } from "formik";
-
-import './CodingLanguages.css'
+import  './CodingLanguages.css'
+import { getCodingLanguages } from './Services.js'
 
 class CodingLanguages extends React.Component  {
+  constructor(props){
+    super(props)
+    this.state = { languageId: null}
+  }
+
+  async mySubmit(id){
+    let result = await getCodingLanguages()
+
+    console.log(result)
+    console.log(result.body)
+  }
+
+
 
   render() {
 
     return(
       <>
+
+        <h1>Coding Languages</h1>
+        <p>
+          select your favorite coding language
+        </p>
         <Formik
-          initialValues={{ name: "", email: "" }}
+          initialValues={{ languageId: null}}
           onSubmit={ (values) => {
-            console.log(values)
+            this.mySubmit(values.id)
           }}>
             <Form>
               <label htmlFor="name">Name</label>
-              <Field name="name" type="text" />
+              <Field name="id" type="radio" value="1" />
               <label htmlFor="email">Email</label>
-              <Field name="email" type="email" />
+              <Field name="id" type="radio" value="2"/>
               <button type="submit">Submit</button>
             </Form>
           </Formik>
