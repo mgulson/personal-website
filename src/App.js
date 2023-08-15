9049b0b8d923abfcb942f48fcf3fe9a6ea676300
Michael Gulson <msgulson@gmail.com>
2023-08-14 19:38:59 -0500

    use hash router

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import CodingLanguages from './components/CodingLanguages/CodingLanguages';
 
 import './App.css';
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 class App extends React.Component {
   render() {
     return(
-      <Routes>
-        <Route path="/" element={<Resume/>} />
-        <Route path="/personal-website" element={<Resume/>} />
-        <Route path="/coding-languages" element={<CodingLanguages/>}/>
-      </Routes>
+      <HashRouter>    
+        <Routes>
+          <Route path="/" element={<Resume/>} />
+          <Route path="/personal-website" element={<Resume/>} />
+          <Route path="/coding-languages" element={<CodingLanguages/>}/>
+        </Routes>
+      </HashRouter>
     )
   }
 }
