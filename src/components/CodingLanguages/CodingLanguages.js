e25f8aa0ed466d7e4650a8aa3c71f3f92595a2ca
Michael Gulson <msgulson@gmail.com>
2023-08-24 18:59:24 -0500

    create coding lanaguages

--- src/components/CodingLanguages/CodingLanguages.js
+++ src/components/CodingLanguages/CodingLanguages.js
@@ -31,37 +31,30 @@ class CodingLanguages extends React.Component  {
         <p>
           select your favorite coding language
         </p>
-        
-        <>
-          {this.state.languages.map((language) => language.name)}
-        </>
 
         <Formik
           initialValues={{ languageId: null}}
           onSubmit={ (values) => {
             this.mySubmit(values.id)
           }}>
-            <Form>
-              <label htmlFor="name">Javascript</label>
-              <Field name="id" type="radio" value="1" />
-              <label htmlFor="email">Java</label>
-              <Field name="id" type="radio" value="2"/>
-              <label htmlFor="name">Python</label>
-              <Field name="id" type="radio" value="3" />
-              <label htmlFor="email">C</label>
-              <Field name="id" type="radio" value="4"/>              
-              <label htmlFor="name">C++</label>
-              <Field name="id" type="radio" value="5" />
-              <label htmlFor="email">Ruby</label>
-              <Field name="id" type="radio" value="6"/>
-              <label htmlFor="name">Go</label>
-              <Field name="id" type="radio" value="7" />
-              <label htmlFor="email">C#</label>
-              <Field name="id" type="radio" value="8"/>
-              <button type="submit">Submit</button>
-            </Form>
-          </Formik>
-        </>
+
+          <Form>
+            {
+              this.state.languages?.map((language, index) =>
+                {
+
+                  return(
+                    <>
+                      <label htmlFor="name">{language.name}</label>
+                      <Field name="id" type="radio" value={index} />
+                    </>
+                  )
+                }
+              )
+            }
+          </Form>
+        </Formik>
+      </>
     )
   }
 }
