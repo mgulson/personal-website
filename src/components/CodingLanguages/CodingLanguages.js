3790fc6c3fb5c72fdda215a2501e2b73710e3d38
Michael Gulson <msgulson@gmail.com>
2023-08-24 19:24:20 -0500

    html loops

--- src/components/CodingLanguages/CodingLanguages.js
+++ src/components/CodingLanguages/CodingLanguages.js
@@ -13,14 +13,24 @@ class CodingLanguages extends React.Component  {
     let result = await getCodingLanguages()
 
     this.setState({ languages: result, selectedLanguageId: null } )
-    console.log(this.state)
+    this.onValueChange = this.onValueChange.bind(this);
+    this.formSubmit = this.formSubmit.bind(this);
   }
 
   async mySubmit(id){
     this.setState({selectedLanguageId: id, languages: this.state.languages})
   }
 
+  onValueChange(event) {
+    this.setState({
+      selectedOption: event.target.value
+    });
+  }
 
+  formSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.selectedOption)
+  }
 
   render() {
 
@@ -32,29 +42,49 @@ class CodingLanguages extends React.Component  {
           select your favorite coding language
         </p>
 
-        <Formik
-          initialValues={{ languageId: null}}
-          onSubmit={ (values) => {
-            this.mySubmit(values.id)
-          }}>
+        <form onSubmit={this.formSubmit}>
+        <div className="radio">
+          <label>
+            Male
+            <input
+              type="radio"
+              value="Male"
+              checked={this.state.selectedOption === "Male"}
+              onChange={this.onValueChange}
+            />
+          </label>
+        </div>
+        <div className="radio">
+          <label>
+            Female
+            <input
+              type="radio"
+              value="Female"
+              checked={this.state.selectedOption === "Female"}
+              onChange={this.onValueChange}
+            />
+          </label>
+        </div>
+        <div className="radio">
+          <label>
+            Other
+            <input
+              type="radio"
+              value="Other"
+              checked={this.state.selectedOption === "Other"}
+              onChange={this.onValueChange}
+            />
+            Other
+          </label>
+        </div>
+        <div>
+          Selected option is : {this.state.selectedOption}
+        </div>
+        <button className="btn btn-default" type="submit">
+          Submit
+        </button>
+      </form>
 
-          <Form>
-            {
-              this.state.languages?.map((language, index) =>
-                {
-
-                  return(
-                    <>
-                      <label htmlFor="name">{language.name}</label>
-                      <Field name="id" type="radio" value={index} />
-                    </>
-                  )
-                }
-              )
-            }
-            <botton type="submit">Submit</botton>
-          </Form>
-        </Formik>
       </>
     )
   }
