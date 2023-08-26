388be37b2aabb84ab7322372a1ed7fc09d5a438a
Michael Gulson <msgulson@gmail.com>
2023-08-26 13:02:31 -0500

    fix router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Resume from './components/Resume/Resume';
 import CodingLanguages from './components/CodingLanguages/CodingLanguages';
-
-
 import './App.css';
 
 import { Routes, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 class App extends React.Component {
   render() {
     return(
-      <Routes basename={'/personal-website'}>
-        <Route path="/" element={<Resume/>} />
-        <Route path="/coding-languages" element={<CodingLanguages/>}/>
-      </Routes>
+      <BrowserRouter basename='/personal-website'>
+        <Routes>
+          <Route path="/" element={<Resume/>} />
+          <Route path="/coding-languages" element={<CodingLanguages/>}/>
+        </Routes>
+      </BrowserRouter>  
     )
   }
 }
