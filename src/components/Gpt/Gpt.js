675f55b188e02f014a79cfaf5281b9de426f3875
Michael Gulson <msgulson@gmail.com>
2023-11-14 21:46:40 -0600

    try and post to replicate

--- src/components/Gpt/Gpt.js
+++ src/components/Gpt/Gpt.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { postGpt } from '../../services/gptBackend';
+
 
 class Gpt extends React.Component  {
+  async componentDidMount(){
+    let result = await postGpt('hello world')
+    console.log(result)
+  }
+  
   render() {
     return(
       <h1>MichaelGpt</h1>
--- src/services/gptBackend.js
+++ src/services/gptBackend.js
@@ -1,15 +1,22 @@
-export let replicateurl = 'https://api.replicate.com/v1/deployments/gullman99/my-llama/predictions'
+import Replicate from "replicate"
 
+export let replicateBaseUrl = 'https://api.replicate.com/v1/deployments/gullman99/my-llama/predictions'
 
-export async function postQuestion(id) {
-  let data = { "language_id": id}
-  fetch(URL + '/votes', {
-    headers: {
-      "Content-Type": "application/json"
-    },
-    method: "POST",
-    mode: 'cors',
-    cache: 'default',
-    body: JSON.stringify(data),
-  }).then((response) => response && response.json())
+export let API_KEY = process.env.REACT_APP_REPLICATE_API_KEY
+
+const replicate = new Replicate({
+  auth: process.env.REACT_APP_REPLICATE_API_KEY,
+});
+
+export async function postGpt(prompt) {
+  let data = { input: { prompt } }
+  let prediction = await replicate.deployments.predictions.create(
+    "gullman99",
+    "my-llama",
+    {
+      data
+    }
+  )
+  prediction = await replicate.wait(prediction)
+  console.log(prediction.output)
 }
