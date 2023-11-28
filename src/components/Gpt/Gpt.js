dc45506128713f44606bab87aaa3b2102ff91eda
Michael Gulson <msgulson@gmail.com>
2023-11-27 21:25:04 -0600

    it works

--- src/components/Gpt/Gpt.js
+++ src/components/Gpt/Gpt.js
@@ -5,7 +5,7 @@ import './Gpt.css'
 class Gpt extends React.Component  {
   constructor(props){
     super(props)
-    this.state = { inputValue: '', showText: null, answer: null}
+    this.state = { inputValue: '', showText: false, answer: null}
     this.formSubmit = this.formSubmit.bind(this)
     this.handleInputChange = this.handleInputChange.bind(this)
   }
@@ -19,30 +19,41 @@ class Gpt extends React.Component  {
   }
 
   async formSubmit(event) {
+    console.log(this.state)
     event.preventDefault();
     let result = await postGpt(this.state.inputValue)
-    console.log(result)
-    let answer = result?.choices[0]?.content
-    console.log(this)
+    let answer = result?.choices[0]?.message?.content
+    console.log(this.state)
     this.setState({showText: true, answer})
+    console.log(this.state)
   }
   
   render() {
     return(
       <>
-      <h1>MichaelGpt</h1>
-      <form onSubmit={this.formSubmit}>
-        <input className='text'
-            type="text"
-            value={this.state.inputValue}
-            onChange={this.handleInputChange}
-            placeholder="Enter text"
-          />
-        <br></br>
-        <button className='form-submit' type="submit">
-          Submit
-        </button>
-      </form>
+      <div className='center-gpt'>
+        <h1>MichaelGpt</h1>
+        <form onSubmit={this.formSubmit}>
+          <input className='text'
+              type="text"
+              value={this.state.inputValue}
+              onChange={this.handleInputChange}
+              placeholder="Enter text"
+            />
+          <br></br>
+          <button className='form-submit' type="submit">
+            Submit
+          </button>
+        </form>
+
+        {
+          this.state.showText ? 
+          <>
+            <p>{this.state.answer}</p>
+          </> :
+          <></>
+        }
+      </div>
     </>
     )
   }
