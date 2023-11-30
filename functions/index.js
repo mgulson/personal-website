89fcd5fc5525b4ceb7befb1685e4b0559db6b009
Michael Gulson <msgulson@gmail.com>
2023-11-29 20:02:44 -0600

    gpt firebase functions

--- functions/index.js
+++ functions/index.js
@@ -7,13 +7,71 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
+const {onRequest, onCall} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
-
+let openapiURL = "https://api.openai.com/v1/chat/completions"
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+exports.openaipost = onRequest(
+  { secrets: ["OPEN_AI_API_KEY"], cors: true },
+  async (req, res) => {
+
+    let API_KEY = process.env.OPEN_AI_API_KEY
+    let prompt = req.body.prompt
+    logger.log(req.body)
+    logger.log(prompt)
+    let data = { 
+      "model": "gpt-3.5-turbo",
+      "messages": [
+           {"role": "system", "content": "You are a helpful assistant"},
+          {"role": "user", "content": prompt }
+          ],
+      "temperature": 0.7 
+    }
+    response = await fetch(openapiURL, {
+      headers: {
+        "Content-Type": "application/json",
+        'Authorization': `Bearer ${API_KEY}`
+      },
+      method: "POST",
+      mode: 'cors',
+      cache: 'default',
+      body: JSON.stringify(data),
+    }).then((response) => response && response.json())
+  
+    res.send(response)
+  }
+)
+
+// exports.openaioncallpost = onCall(
+//   { secrets: ["OPEN_AI_API_KEY"], cors: true },
+//   async (req, res) => {
+
+//     let API_KEY = process.env.OPEN_AI_API_KEY
+//     let prompt = req.body.prompt
+//     logger.log(req.body)
+//     logger.log(prompt)
+//     let data = { 
+//       "model": "gpt-3.5-turbo",
+//       "messages": [
+//            {"role": "system", "content": "You are a helpful assistant"},
+//           {"role": "user", "content": prompt }
+//           ],
+//       "temperature": 0.7 
+//     }
+//     response = await fetch(openapiURL, {
+//       headers: {
+//         "Content-Type": "application/json",
+//         'Authorization': `Bearer ${API_KEY}`
+//       },
+//       method: "POST",
+//       mode: 'cors',
+//       cache: 'default',
+//       body: JSON.stringify(data),
+//     }).then((response) => response && response.json())
+  
+//     res.send(response)
+//   }
+// )
